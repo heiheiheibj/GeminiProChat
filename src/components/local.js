38d4748bf51ac70b0ef4ab1ca9560f66b382cac0
heiheiheibj <hater@163.com>
2023-12-26 22:11:19 +0800

    Update local.js

--- src/components/local.js
+++ src/components/local.js
@@ -1,5 +1,5 @@
 var element = document.querySelector('.gpt-title');  
-element.innerHTML = "马发发";
+element.innerHTML = "";
 var element = document.querySelector('.gpt-subtitle');  
 element.innerHTML = "痛饮狂歌空度日,飞扬跋扈为谁雄";
-alert("a");
+//alert("a");
